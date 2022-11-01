33cf228bd2f8b7a1cc8f3a18a4033f12e0eb97e4
AntimonyIQ <antimonyiq@gmail.com>
2022-11-01 02:21:25 +0100

    test commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
 var width = window.innerWidth;
 /// alert(width);
+var MobileNavContent = document.getElementById('MobileNavContent');
+var MenuBtn = document.getElementById('MenuBtn');
+var MenuClose = document.getElementById('MenuClose');
+
+function MenuNavigation() {
+    // alert(width);
+    MobileNavContent.style.display = 'block';
+    MenuBtn.style.display = 'none';
+    MenuClose.style.display = 'block';
+}
+
+function CloseNavigation() {
+    MobileNavContent.style.display = 'none';
+    MenuBtn.style.display = 'block';
+    MenuClose.style.display = 'none';
+}
